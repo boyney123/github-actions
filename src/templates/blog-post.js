da1b77043ff9fa61fdc1f1930543828c2e7247f1
David Boyne <davidboyne123@hotmail.co.uk>
2019-01-25 13:15:04 +0000

    Small design change, added missing style sheet to blog

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -12,7 +12,24 @@ export default function Template({ data }) {
   const { title, subtitle, github_url, twitter } = frontmatter
   return (
     <Layout className="blog-post-container">
-      <Helmet title={`Github-Actions - ${frontmatter.title}`} />
+      <Helmet title={`Github-Actions - ${frontmatter.title}`}>
+        <script
+          async
+          src="https://platform.twitter.com/widgets.js"
+          charset="utf-8"
+        />
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
+          integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP"
+          crossorigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css"
+        />
+        <html lang="en" />
+      </Helmet>
       <div className="blog-post container">
         <h1>{title}</h1>
         <p>{subtitle}</p>
